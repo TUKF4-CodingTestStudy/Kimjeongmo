e9494a336e4734ceb5beb61ddd9493b74c4377a3
Jmmk <jmk525@gmail.com>
2023-03-23 18:57:00 +0900

    11399 풀이

---
+++ baekjoon/11399_ATM.py
@@ -0,0 +1,11 @@
+people = int(input())
+time_list = list(map(int, input().split()))
+time_list.sort()
+num = 0
+total_time = 0
+
+for i in range(0,people):
+    num += time_list[i]
+    total_time += num
+
+print(total_time)
